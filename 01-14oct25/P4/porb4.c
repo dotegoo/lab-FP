c9d8c67eba1d1a08d83eca68bd14ae999fcc5ffb
Teodor Dobromir <teodordobromir@gmail.com>
2025-10-14 15:37:46 +0300

    fix P4

--- 01-14oct25/P4/porb4.c
+++ 01-14oct25/P4/porb4.c
@@ -3,11 +3,13 @@
 int main(){
 
     int ora,min,sec,ora1,ora2,min1,min2,sec1,sec2;
+    printf("Introduceti ora, minutele si secundele pentru primul interval de timp: ");
     scanf("%d",&ora1);
-    scanf("%d",&ora2);
     scanf("%d",&min1);
-    scanf("%d",&min2);
     scanf("%d",&sec1);
+    printf("\nIntroduceti ora, minutele si secundele pentru al doilea interval de timp: ");
+    scanf("%d",&ora2);
+    scanf("%d",&min2);
     scanf("%d",&sec2);
     ora=ora1+ora2;
     min=min1+min2;
